55d8bd839a267bfff6ec43d2c19fece13b311cdc
jkeum-dev <jon9eun@gmail.com>
2024-05-23 17:51:52 +0900

    feat : 게시물 좋아요 취소

--- src/main/java/com/ssu/goodplassu/cheer/openapi/CheerApi.java
+++ src/main/java/com/ssu/goodplassu/cheer/openapi/CheerApi.java
@@ -1,6 +1,6 @@
 package com.ssu.goodplassu.cheer.openapi;
 
-import com.ssu.goodplassu.cheer.dto.request.CheerOnRequest;
+import com.ssu.goodplassu.cheer.dto.request.CheerUpdateRequest;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -19,5 +19,15 @@ public interface CheerApi {
 	@ApiResponse(responseCode = "400", description = "게시글 좋아요 실패")
 	@ApiResponse(responseCode = "500", description = "서버 에러")
 	@PostMapping("/like")
-	public ResponseEntity<?> setCheerOn(@RequestBody final CheerOnRequest cheerOnRequest);
+	public ResponseEntity<?> setCheerOn(@RequestBody final CheerUpdateRequest cheerUpdateRequest);
+
+	@Operation(
+			summary = "게시글에 좋아요 취소",
+			description = "좋아요 취소\n좋아요 누르는 게시물 id와 로그인 한 유저 id 필요(로그인하지 않은 경우 null)"
+	)
+	@ApiResponse(responseCode = "200", description = "게시글 좋아요 취소 성공")
+	@ApiResponse(responseCode = "400", description = "게시글 좋아요 취소 실패")
+	@ApiResponse(responseCode = "500", description = "서버 에러")
+	@PostMapping("/unlike")
+	public ResponseEntity<?> setCheerOff(@RequestBody final CheerUpdateRequest cheerUpdateRequest);
 }
