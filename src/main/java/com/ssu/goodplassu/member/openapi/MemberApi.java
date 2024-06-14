9f91f2d0bc1b0b0708c070bb01009872959f52c6
jkeum <jon9eun@gmail.com>
2024-06-14 17:35:16 +0900

    feat : Swagger에 응답 데이터 예시 추가 (#61)

--- src/main/java/com/ssu/goodplassu/member/openapi/MemberApi.java
+++ src/main/java/com/ssu/goodplassu/member/openapi/MemberApi.java
@@ -1,7 +1,12 @@
 package com.ssu.goodplassu.member.openapi;
 
+import com.ssu.goodplassu.common.dto.ResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,10 +16,48 @@ public interface MemberApi {
 
 	@Operation(
 			summary = "이달의 선행왕 리스트",
-			description = "이달의 month_point가 가장 높은 4명"
+			description = "month_point가 가장 높은 4명"
 	)
-	@ApiResponse(responseCode = "200", description = "이달의 선행왕 4명 조회 성공")
-	@ApiResponse(responseCode = "500", description = "서버 에러")
+	@ApiResponses(value = {
+			@ApiResponse(
+					responseCode = "200",
+					description = "이달의 선행왕 4명 조회 성공",
+					content = @Content(
+							mediaType = "application/json",
+							schema = @Schema(implementation = ResponseDto.class),
+							examples = @ExampleObject(value =
+									"{" +
+											"\"result\": true," +
+											"\"status\": 200," +
+											"\"success\": \"이달의 선행왕 목록을 불러왔습니다.\"," +
+											"\"data\": [" +
+													"{\n" +
+													"      \"writer_name\": \"Alice Johnson\",\n" +
+													"      \"writer_profile\": \"portrait_url_here\",\n" +
+													"      \"month_point\": 80\n" +
+													"    },\n" +
+													"    {\n" +
+													"      \"writer_name\": \"Jane Smith\",\n" +
+													"      \"writer_profile\": \"portrait_url_here\",\n" +
+													"      \"month_point\": 75\n" +
+													"    },\n" +
+													"    {\n" +
+													"      \"writer_name\": \"Bob Brown\",\n" +
+													"      \"writer_profile\": \"portrait_url_here\",\n" +
+													"      \"month_point\": 60\n" +
+													"    },\n" +
+													"    {\n" +
+													"      \"writer_name\": \"John Doe\",\n" +
+													"      \"writer_profile\": \"portrait_url_here\",\n" +
+													"      \"month_point\": 50\n" +
+													"    }\n" +
+											"]" +
+									"}"
+							)
+					)
+			),
+			@ApiResponse(responseCode = "500", description = "서버 에러")
+	})
 	@GetMapping("/month")
 	public ResponseEntity<?> getHighestMonthPoint();
 
@@ -22,8 +65,46 @@ public interface MemberApi {
 			summary = "전체 선행왕 리스트",
 			description = "total_point가 가장 높은 4명"
 	)
-	@ApiResponse(responseCode = "200", description = "전체 선행왕 4명 조회 성공")
-	@ApiResponse(responseCode = "500", description = "서버 에러")
+	@ApiResponses(value = {
+			@ApiResponse(
+					responseCode = "200",
+					description = "전체 선행왕 4명 조회 성공",
+					content = @Content(
+							mediaType = "application/json",
+							schema = @Schema(implementation = ResponseDto.class),
+							examples = @ExampleObject(value =
+									"{" +
+											"\"result\": true," +
+											"\"status\": 200," +
+											"\"success\": \"전체 선행왕 목록을 불러왔습니다.\"," +
+											"\"data\": [" +
+													"{\n" +
+													"      \"writer_name\": \"Alice Johnson\",\n" +
+													"      \"writer_profile\": \"portrait_url_here\",\n" +
+													"      \"total_point\": 200\n" +
+													"    },\n" +
+													"    {\n" +
+													"      \"writer_name\": \"Bob Brown\",\n" +
+													"      \"writer_profile\": \"portrait_url_here\",\n" +
+													"      \"total_point\": 180\n" +
+													"    },\n" +
+													"    {\n" +
+													"      \"writer_name\": \"Jane Smith\",\n" +
+													"      \"writer_profile\": \"portrait_url_here\",\n" +
+													"      \"total_point\": 150\n" +
+													"    },\n" +
+													"    {\n" +
+													"      \"writer_name\": \"John Doe\",\n" +
+													"      \"writer_profile\": \"portrait_url_here\",\n" +
+													"      \"total_point\": 100\n" +
+													"    }" +
+											"]" +
+									"}"
+							)
+					)
+			),
+			@ApiResponse(responseCode = "500", description = "서버 에러")
+	})
 	@GetMapping("/total")
 	public ResponseEntity<?> getHighestTotalPoint();
 }
