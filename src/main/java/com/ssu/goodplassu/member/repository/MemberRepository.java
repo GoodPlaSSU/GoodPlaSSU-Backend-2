4a3912b66a8aa2a03aa85e27178c00ea8e50e9a4
jkeum <jon9eun@gmail.com>
2024-06-23 21:44:36 +0900

    feat : 매달 1일에 month point 0으로 초기화 - 날짜 테스트 (#65)

--- src/main/java/com/ssu/goodplassu/member/repository/MemberRepository.java
+++ src/main/java/com/ssu/goodplassu/member/repository/MemberRepository.java
@@ -2,6 +2,7 @@ package com.ssu.goodplassu.member.repository;
 
 import com.ssu.goodplassu.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,4 +11,6 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 	Optional<Member> findByEmail(String email);
 	List<Member> findTop4ByOrderByMonthPointDesc();
 	List<Member> findTop4ByOrderByTotalPointDesc();
+	@Query("UPDATE Member m SET m.monthPoint = 0")
+	void resetAllMonthPoints();
 }
---
+++ src/main/java/com/ssu/goodplassu/member/scheduler/MonthPointScheduler.java
@@ -0,0 +1,17 @@
+package com.ssu.goodplassu.member.scheduler;
+
+import com.ssu.goodplassu.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class MonthPointScheduler {
+	private final MemberService memberService;
+
+	@Scheduled(cron = "0 0 0 24 * ?") // 매달 24일 자정에 실행, 초 분 시 일 월 요일
+	public void scheduleResetMonthlyPoints() {
+		memberService.resetMonthPointAllMembers();
+	}
+}
