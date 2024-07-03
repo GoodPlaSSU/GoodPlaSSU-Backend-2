27ed6e401f441c3aad31a68b8bf911ce0ac6b171
jkeum-dev <jon9eun@gmail.com>
2024-07-03 14:45:29 +0900

    feat : 인증이 필요한 api 추가

--- src/main/java/com/ssu/goodplassu/common/config/SecurityConfig.java
+++ src/main/java/com/ssu/goodplassu/common/config/SecurityConfig.java
@@ -31,11 +31,16 @@ public class SecurityConfig {
 				.csrf(c -> c.disable())			// REST API를 보호하는 데 자주 사용되는 설정으로, 상태를 유지하지 않는 API 서버에서는 CSRF 보호를 비활성화하는 것이 일반적임
 				.sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))	// 세션을 사용하지 않고, 각 요청을 독립적으로 인증하는 방식을 선택. JWT 사용 시 필수적인 설정. 스프링 시큐리티가 세션을 생성하지 않고, 있어도 사용하지 않겠다.
 				.authorizeHttpRequests(a -> a	// 요청에 대한 접근 제어 설정
-								.requestMatchers(new AntPathRequestMatcher("/api/boards/**", HttpMethod.POST.name())).hasRole(Role.USER.name())
-								.requestMatchers(new AntPathRequestMatcher("/api/boards/**", HttpMethod.DELETE.name())).hasRole(Role.USER.name())
-								.requestMatchers("/api/comments/**").hasRole(Role.USER.name())
-								.requestMatchers("/api/cheers/**").hasRole(Role.USER.name())
-								.requestMatchers(
+						.requestMatchers(new AntPathRequestMatcher("/api/boards/**", HttpMethod.POST.name())).hasRole(Role.USER.name())
+						.requestMatchers(new AntPathRequestMatcher("/api/boards/**", HttpMethod.DELETE.name())).hasRole(Role.USER.name())
+						.requestMatchers("/api/comments/**").hasRole(Role.USER.name())
+						.requestMatchers("/api/cheers/**").hasRole(Role.USER.name())
+						.requestMatchers("/api/members/posts").hasRole(Role.USER.name())
+						.requestMatchers("/api/members/likes/posts").hasRole(Role.USER.name())
+						.requestMatchers("/api/members/comments/posts").hasRole(Role.USER.name())
+						.requestMatchers("/api/members/info").hasRole(Role.USER.name())
+						.requestMatchers("/api/auth/logout").hasRole(Role.USER.name())
+						.requestMatchers(
 								new MvcRequestMatcher(introspector, "/h2-console/**"),
 								new AntPathRequestMatcher("/**")
 						).permitAll()
